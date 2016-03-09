84f6181edaaf173b391db29ffef87c6cb67f9a53
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-03-08 21:40:59 -0500

    Improve legibility.

--- app/src/Test/AbstractTestCase.php
+++ app/src/Test/AbstractTestCase.php
@@ -16,8 +16,7 @@ abstract class AbstractTestCase extends WebTestCase
     return $app->load();
   }
 
-  protected function _testRequest($method, $url, array $headers = [],
-    $body = '', $expectedStatus = Response::HTTP_OK)
+  protected function _testRequest($method, $url, array $headers = [], $body = '', $expectedStatus = Response::HTTP_OK)
   {
     $client = $this->createClient();
     $crawler = $client->request($method, $url, [], [], $headers, $body);
@@ -25,19 +24,21 @@ abstract class AbstractTestCase extends WebTestCase
     $this->assertEquals(
       $client->getResponse()->getStatusCode(),
       $expectedStatus,
-      sprintf('Response is %s: %s', $expectedStatus,
-        json_encode($client->getResponse()->getContent()))
+      sprintf('Response is %s: %s', $expectedStatus, json_encode($client->getResponse()->getContent()))
     );
 
     return [$client, $crawler];
   }
 
-  protected function _testJson($method, $url, array $body = [],
-    $expectedStatus = Response::HTTP_OK)
+  protected function _testJson($method, $url, array $body = [], $expectedStatus = Response::HTTP_OK)
   {
-    list($client, $crawler) = $this->_testRequest($method, $url, [
-      'CONTENT_TYPE' => 'application/json'
-    ], json_encode($body), $expectedStatus);
+    list($client, $crawler) = $this->_testRequest(
+      $method,
+      $url,
+      ['CONTENT_TYPE' => 'application/json'],
+      json_encode($body),
+      $expectedStatus
+    );
 
     $res = json_decode($client->getResponse()->getContent(), true);
     $this->assertNotEquals($res, null, 'Json decoding succeeds');
